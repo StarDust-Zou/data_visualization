0bff8af5524bbbace4fcc58bcf6d23825c7c4f4a
StarDust-Zou <70910029+StarDust-Zou@users.noreply.github.com>
2022-06-01 23:45:08 +0800

    first modified

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="app">
+        <Chart1></Chart1>
+<!--        <Chart2></Chart2>-->
+        <Chart3></Chart3>
+    </div>
+</template>
+
+<script>
+import Chart1 from "@/components/Chart1";
+import Chart2 from "@/components/Chart2";
+import Chart3 from "@/components/Chart3";
+
+export default {
+    name: 'App',
+    components: {
+        Chart1,
+        Chart2,
+        Chart3,
+    },
+
+}
+
+</script>
+
+<style>
+#app {
+    background:url("./assets/bgimg.jpg");
+    width:100%;
+    height:100%;
+    position:fixed;
+    background-size:100% 100%;
+}
+
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import echarts from "echarts"
+
+createApp(App).mount('#app')
